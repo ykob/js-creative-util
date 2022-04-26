d8321c7004bb3680a7ada3b2336be2823dcdd185
ykob <info@tplh.net>
2022-04-26 22:45:44 +0900

    clean up

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -8,7 +8,7 @@ const mag = (v: Vector3): number => {
   return Math.sqrt(Math.pow(v[0], 2) + Math.pow(v[1], 2) + Math.pow(v[2], 2))
 }
 
-const multi = (v: Vector3, l: number): Vector3 => {
+const mult = (v: Vector3, l: number): Vector3 => {
   return [v[0] * l, v[1] * l, v[2] * l]
 }
 
@@ -21,4 +21,4 @@ const sub = (v1: Vector3, v2: Vector3): Vector3 => {
   return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]
 }
 
-export { add, mag, multi, normalize, sub }
+export { add, mag, mult, normalize, sub }
