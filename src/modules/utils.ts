60da1a3a77f963a1aa15a17f38babf0eb9bd7ffd
ykob <info@tplh.net>
2022-04-26 22:45:22 +0900

    added `multi` function on util.

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -8,13 +8,17 @@ const mag = (v: Vector3): number => {
   return Math.sqrt(Math.pow(v[0], 2) + Math.pow(v[1], 2) + Math.pow(v[2], 2))
 }
 
+const multi = (v: Vector3, l: number): Vector3 => {
+  return [v[0] * l, v[1] * l, v[2] * l]
+}
+
 const normalize = (v: Vector3): Vector3 => {
-  const length = mag(v)
-  return [v[0] / length, v[1] / length, v[2] / length]
+  const l = mag(v)
+  return [v[0] / l, v[1] / l, v[2] / l]
 }
 
 const sub = (v1: Vector3, v2: Vector3): Vector3 => {
   return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]
 }
 
-export { add, mag, normalize, sub }
+export { add, mag, multi, normalize, sub }
