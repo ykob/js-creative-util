3c4ced72b456b4e62f966925841d5bb7ee16a08b
ykob <info@tplh.net>
2022-04-27 21:41:33 +0900

    added the feature of the drag function.

--- src/modules/drag.ts
+++ src/modules/drag.ts
@@ -1,5 +1,10 @@
-const drag = (): any => {
-  return
+import { Vector3 } from "../models"
+import { mag, mult, normalize } from "./utils.js"
+
+const drag = (acceleration: Vector3, c: number): Vector3 => {
+  const length = c * mag(acceleration)
+  const dir = mult(normalize(acceleration), -1)
+  return mult(dir, length)
 }
 
 export default drag
