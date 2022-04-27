8a4549d733ca9c71ba4804137b4ddfc1ddcbcdc7
ykob <info@tplh.net>
2022-04-27 20:02:05 +0900

    added the demo of Hool class.

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,5 +1,20 @@
 import Force3 from './Force3.js'
+import { Hook } from '../index.js'
 
+const elmCircle: HTMLElement | null = document.querySelector('.circle')
 const force3 = new Force3()
+const hook = new Hook(0.1, 10)
 
-console.log(force3)
+const update = () => {
+  hook.setAnchor([50, 50, 0])
+  const hookForce = hook.update(force3.velocity)
+  force3.applyForce(hookForce)
+  force3.updateVelocity()
+  elmCircle!.style.transform = `translate3d(${force3.velocity[0]}px, ${-force3.velocity[1]}px, ${force3.velocity[2]}px)`
+  requestAnimationFrame(update)
+}
+const start = () => {
+  update()
+}
+
+start()
