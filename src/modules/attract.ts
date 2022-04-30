b888b77cd1bedd9f34423c9bc3233da2b4cb9fc8
ykob <info@tplh.net>
2022-04-30 14:53:52 +0900

    added the attract function.

--- src/modules/attract.ts
+++ src/modules/attract.ts
@@ -1,5 +1,23 @@
-const attract = (): any => {
-  return
+import { Vector3 } from '../models'
+import { mag, mult, normalize, sub } from './utils.js'
+
+const attract = (
+  vSelf: Vector3,
+  mSelf: number,
+  vTarget: Vector3,
+  mTarget: number,
+  distMin: number,
+  distMax: number
+): Vector3 => {
+  const v = sub(vTarget, vSelf)
+  const dist = Math.min(Math.max(mag(v), distMin), distMax)
+  const dir = normalize(v)
+  if (dist === 0) {
+    return [0, 0, 0]
+  } else {
+    const str = (1 * mSelf * mTarget) / Math.pow(dist, 2)
+    return mult(dir, str)
+  }
 }
 
 export default attract
